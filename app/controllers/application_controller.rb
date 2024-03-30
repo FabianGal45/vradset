a0814447e2a83b42271abe07a6ae59045a44650b
galfa <fabiangal.45@gmail.com>
2024-03-30 16:37:50 +0000

    Implemented role selection upon Sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # Allow the use of role variable in as per devise documentation
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Enum used to save the string into an integer in the database
+  enum role: { admin: 0, advertiser: 1, developer: 2 }
 end
