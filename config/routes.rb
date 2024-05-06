1e26cc86e671e57b9994059402b22d5ea942f6f5
galfa <fabiangal.45@gmail.com>
2024-05-06 17:07:44 +0100

    Changed the route random_image to get_image

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   # Route used to trigger the random image method from the Advertisements_controller.rb where it will retun a random image
-  get 'random_image', to: 'advertisements#random_image'
+  get 'get_image', to: 'advertisements#get_image'
 
   # Defines the root path route ("/")
   root "vrassets#index"
