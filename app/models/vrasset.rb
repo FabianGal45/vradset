d1b36cb9b45b234cfe88018eb70ccc6c91650adf
galfa <fabiangal.45@gmail.com>
2024-05-08 15:22:46 +0100

    Vrassets - All text fields are now required to create a VR asset

--- app/models/vrasset.rb
+++ app/models/vrasset.rb
@@ -1,4 +1,5 @@
 class Vrasset < ApplicationRecord
     has_one_attached :file # https://edgeguides.rubyonrails.org/active_storage_overview.html#has-one-attached
     has_one_attached :image # This is the image of the asset
+    validates :title, :description, :file, :image, presence: true # Ensures that the following information has been provided
 end
