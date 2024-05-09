001607a091ebbac82474bf4551b8773c472812c6
galfa <fabiangal.45@gmail.com>
2024-05-09 14:32:13 +0100

    Created the Tutorial page and route

---
+++ app/controllers/tutorial_controller.rb
@@ -0,0 +1,5 @@
+class TutorialController < ApplicationController
+    # GET /tutorial or /tutorial.json
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,12 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
   # Route used to trigger the random image method from the Advertisements_controller.rb where it will retun a random image
   get 'get_image', to: 'advertisements#get_image'
+
+  # Route for the tutorial page
+  get 'tutorial', to: 'tutorial#index'
 
   # Defines the root path route ("/")
   root "vrassets#index"
