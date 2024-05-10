49706ffb19fc0c092610c555671ace92c4343828
galfa <fabiangal.45@gmail.com>
2024-05-10 21:15:17 +0100

    Fixed and passed advertisements_controller_test.rb

--- test/controllers/advertisements_controller_test.rb
+++ test/controllers/advertisements_controller_test.rb
@@ -5,6 +5,7 @@ class AdvertisementsControllerTest < ActionDispatch::IntegrationTest
     @user = users(:admin) #grabs the admin from the users fixtures
     sign_in @user #This makes use of the Devise helper to sign in the user.
     @advertisement = advertisements(:one)
+    @file = fixture_file_upload("Advertisement_test.png", "application/png")
   end
 
   test "user is signed in" do
@@ -24,10 +25,19 @@ class AdvertisementsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create advertisement" do
+    # The advertisement count should increase
     assert_difference("Advertisement.count") do
-      post advertisements_url, params: { advertisement: { description: @advertisement.description, title: @advertisement.title, url: @advertisement.url } }
+      post advertisements_url, params: { advertisement: {
+          description: "test",
+          title: "test",
+          url: "test",
+          file: fixture_file_upload("Advertisement_test.png", "image/png"),
+          check_asai_all: '1',
+          check_asai_children: '1'
+        }}
     end
 
+    # When an advertisement gets created it will redirect to it's show page. This is what is being checked here
     assert_redirected_to advertisement_url(Advertisement.last)
   end
 
@@ -42,7 +52,14 @@ class AdvertisementsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update advertisement" do
-    patch advertisement_url(@advertisement), params: { advertisement: { description: @advertisement.description, title: @advertisement.title, url: @advertisement.url } }
+    patch advertisement_url(@advertisement), params: { advertisement: {
+      description: @advertisement.description,
+      title: @advertisement.title,
+      url: @advertisement.url,
+      file: fixture_file_upload("Advertisement_test.png", "image/png"),
+      check_asai_all: '1',
+      check_asai_children: '1'
+    } }
     assert_redirected_to advertisement_url(@advertisement)
   end
 
--- test/controllers/tutorial_controller_test.rb
+++ test/controllers/tutorial_controller_test.rb
@@ -4,4 +4,9 @@ class TutorialControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get index" do
+    get tutorial_url
+    assert_response :success
+  end
 end
