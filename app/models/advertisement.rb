5d492f712ec043a195bb1ff0b64ec056a7fe8841
galfa <fabiangal.45@gmail.com>
2024-05-10 09:55:05 +0100

    Limited the file uploads to only accepted types PNG and JPEG

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -5,4 +5,13 @@ class Advertisement < ApplicationRecord
     attr_accessor :check_asai_all, :check_asai_children # This is a virtual attribute that allows the variables to be called upon but not saved in the database
     validates :check_asai_all, acceptance: { message: "- You must comply with all ASAI code of standards" }
     validates :check_asai_children, acceptance: { message: "- You must comply with ASAI code of standards for children" }
+
+    #Checks that the image type uploaded is being supported by the application (JPEG & PNG)
+    validate :correct_image_type
+
+    def correct_image_type
+        if file.attached? && !file.content_type.in?(%w(image/jpeg image/png))
+            errors.add(:file, 'must be a JPEG, OR PNG')
+        end
+    end
 end
